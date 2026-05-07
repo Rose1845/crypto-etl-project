755823902554b597f450e4391638ceab9f7d348c
Rose1845 <odhiamborose466@gmail.com>
2026-05-07 14:51:55 +0300

    Add functionality to retrieve coin data from CoinPaprika API
    - Removed unused imports for pandas and load_dotenv
    - Introduced a list of coin IDs to fetch data for Bitcoin, Ethereum, Solana, and XRP
    - Implemented API calls to CoinPaprika for each coin and added success/failure logging

--- crypto_etl.py
+++ crypto_etl.py
@@ -1,7 +1,19 @@
 import requests
-import pandas as pd
+# import pandas as pd
 
 import os
-from load_dotenv import load_dotenv
+# from load_dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+COINS = ["btc-bitcoin", "eth-ethereum", "sol-solana", "xrp-xrp"]
+
+
+for coin_id in COINS:
+    url = f"https://api.coinpaprika.com/v1/coins/{coin_id}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(f"Sucessfully Retrieved data for coin {coin_id}")
+    else:
+        print(f"Failed to retruve data")
